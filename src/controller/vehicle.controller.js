865e775719d316d7439bf69c241f0e9b9dfd3dc9
KIRAN-MISKIN <kiranm121998@gmail.com>
2025-11-20 12:04:59 +0530

    Bug fixed

--- src/controller/vehicle.controller.js
+++ src/controller/vehicle.controller.js
@@ -78,7 +78,11 @@ exports.parkVehicle = async (req, res) => {
 
 				await ParkingFloor.findByIdAndUpdate(
 					floor._id,
-					{ $inc: { 'capacities.vehicleType': -1 } }
+					{
+						$inc: {
+							[`capacities.${vehicleType}`]: -1
+						}
+					}
 				);
 				break;
 			}
@@ -153,7 +157,7 @@ exports.unparkVehicle = async (req, res) => {
 
 		res.json({ ticketId: session._id, exitAt: session.exitAt, fee });
 	} catch (error) {
-		res.status(500).json({ error: error.message });
+		res.status(400).json({ error: "Something went wrong" });
 	}
 };
 
@@ -200,7 +204,7 @@ exports.getParkedDetails = async (req, res) => {
 			{ path: "spotId" }
 		]);
 		const statusActive = alldetailes.filter(session => session.status === 'active');
-		const detailes = statusActive.map((session)=>{
+		const detailes = statusActive.map((session) => {
 			return (
 				{
 					ticketId: session._id,
